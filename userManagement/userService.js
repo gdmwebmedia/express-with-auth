2e7f9275736ae225457c59914e40ca4f244d39b9
Mihai Gheorghe <mihai.gheorghe@gdm.ro>
2024-10-29 19:58:44 +0200

    start of register

--- userManagement/userService.js
+++ userManagement/userService.js
@@ -1,8 +1,34 @@
+const db = require('../db_config/dbInit')
+
 const getAllUsers = (req, res) => {
     res.send('you want to get all users')
 } 
-const registerUser = (req, res) => {
-    res.send('you want to register as a user')
+const registerUser = async (req, res) => {
+    const { email, password } = req.body
+
+    const newUser = {
+        email: email,
+        password: password
+    }
+    try {
+        const addedUser = await db.collection('users').add(newUser)
+
+        // Get the data of the newly created document
+        const userDoc = await addedUser.get()
+        const userData = userDoc.data()
+        
+        console.log('New user ID:', addedUser.id)
+        console.log('New user data:', userData)
+
+        res.status(201).json({
+            id: addedUser.id,
+            ...userData
+        })
+
+    } catch(error) {
+        res.status(500).send(JSON.stringify(error))
+    }
+
 } 
 const loginUser = (req, res) => {
     res.send('you want to login')
